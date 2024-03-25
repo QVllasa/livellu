83db7c5ce03ab2c11074de02863169da44c824d9
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2024-03-25 18:01:32 +0100

    wip

--- api/config/plugins.ts
+++ api/config/plugins.ts
@@ -1,22 +1,33 @@
-
-export default ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
+export default ({env}) => ({
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
       },
     },
-  },
-  "content-versioning": {
-    enabled:  true,
-  },
-}
+    "content-versioning": {
+      enabled: true,
+    },
+    'schemas-to-ts': {
+      enabled: false,
+      config: {
+        acceptedNodeEnvs: ["development"],
+        commonInterfacesFolderName: 'schemas-to-ts',
+        alwaysAddEnumSuffix: false,
+        alwaysAddComponentSuffix: false,
+        usePrettierIfAvailable: true,
+        logLevel: 2,
+        destinationFolder: undefined,
+      }
+    },
+  }
 );
