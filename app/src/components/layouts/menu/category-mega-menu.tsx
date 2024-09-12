264db3f12f1208fd41f1d858091f4c3ebfea40d3
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2024-09-12 12:39:59 +0200

    fix categories

--- app/src/components/layouts/menu/category-mega-menu.tsx
+++ app/src/components/layouts/menu/category-mega-menu.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {Category} from "@/types";
 import {capitalize} from "lodash";
-import {NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuList, NavigationMenuTrigger,} from "@/shadcn/components/ui/navigation-menu"
+import {NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle,} from "@/shadcn/components/ui/navigation-menu"
 import Link from "next/link";
 import {useAtom} from "jotai/index";
 import {allCategoriesAtom} from "@/store/filters";
@@ -12,10 +12,7 @@ export function CategoryMegaMenu() {
     const [allCategories] = useAtom(allCategoriesAtom);
     const [categories, setCategories] = useState<Category[]>([]);
     useEffect(() => {
-
-            setCategories(allCategories);
-            console.log("allCategories", allCategories)
-
+        setCategories(allCategories);
     }, [allCategories]);
 
     return (
@@ -25,20 +22,19 @@ export function CategoryMegaMenu() {
                     <NavigationMenuItem key={index + (category?.name ?? '')}>
                         <NavigationMenuTrigger>
                             <Link href={`/${category?.slug}`} className={'flex items-center justify-center'}>
-                                <Icon name={category?.lucide_icon} className="w-4 h-4 mr-2" />
+                                <Icon name={category?.lucide_icon} className="w-4 h-4 mr-2"/>
                                 {capitalize(category?.name)}
                             </Link>
                         </NavigationMenuTrigger>
-                        <NavigationMenuContent className="w-full lg:w-auto" >
+                        <NavigationMenuContent className="w-full lg:w-auto">
                             <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[980px] lg:grid-cols-[.75fr_1fr] z-30">
                                 {category?.child_categories?.map((childCategory) => (
                                     <li key={childCategory?.name}>
-                                    <Link href={`/${category?.slug}/${childCategory?.slug ?? '/'}` } className={"block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-gray-100 hover:text-gray-foreground focus:bg-gray focus:text-gray-foreground"}>
-                                <div className="text-sm font-medium leading-none">{capitalize(childCategory?.name)}</div>
-
-                            </Link>
-
-                        </li>
+                                        <Link  legacyBehavior passHref href={`/${category?.slug}/${childCategory?.slug ?? '/'}`}
+                                              className={"block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-gray-100 hover:text-gray-foreground focus:bg-gray focus:text-gray-foreground"}>
+                                            <NavigationMenuLink  className={navigationMenuTriggerStyle()}>{capitalize(childCategory?.name)}</NavigationMenuLink>
+                                        </Link>
+                                    </li>
                                 ))}
 
                             </ul>
