542472380bce80d1e4c5268ede40198458349891
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2024-10-11 00:33:21 +0200

    wip

--- app/src/components/products/products-grid-mobile.tsx
+++ app/src/components/products/products-grid-mobile.tsx
@@ -1,11 +1,12 @@
 import {useRouter} from "next/router";
 import ProductCard from "@/components/products/cards/product-card";
 import {Product} from "@/types";
+import {Button} from "@/shadcn/components/ui/button";
 import {ReloadIcon} from "@radix-ui/react-icons";
 import Link from "next/link";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {fetchProducts} from "@/framework/product";
-import InfiniteScroll from 'react-infinite-scroll-component';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface ProductsGridProps {
     initialProducts: Product[];
@@ -23,37 +24,28 @@ export const ProductsGridMobile = ({
                                        initialFilters,
                                    }: ProductsGridProps) => {
     const router = useRouter();
-    const [products, setProducts] = useState<Product[]>(initialProducts);
+    const [products, setProducts] = useState<Product[]>([]);
     const [page, setPage] = useState<number>(initialPage);
-    const [hasMore, setHasMore] = useState<boolean>(true);
-    const [loadingMore, setLoadingMore] = useState<boolean>(false);
+    const [hasMore, setHasMore] = useState<boolean>(true); // For infinite scroll
     const [filters, setFilters] = useState<any>(initialFilters);
+    const [loadingMore, setLoadingMore] = useState<boolean>(false);
+    const [autoLoadCount, setAutoLoadCount] = useState<number>(0);
+    const [isLoading, setIsLoading] = useState(false);
 
-    // Fetch the next page of products when needed
-    const loadMoreProducts = async () => {
-        if (loadingMore || page >= pageCount) return;
+    useEffect(() => {
+        const queryPage = parseInt(router.query.page as string) || 1;
+        setPage(queryPage);
+    }, [router.query.page]);
 
-        setLoadingMore(true);
+    const maxCount = 5;
 
-        try {
-            const nextPage = page + 1;
-            const updatedFilters = { ...filters, page: nextPage };
-            const { data } = await fetchProducts(updatedFilters);
+    useEffect(() => {
+        setProducts(initialProducts);
+    }, [initialProducts]);
 
-            // Append new products to the existing list
-            setProducts((prevProducts) => [...prevProducts, ...data]);
-            setPage(nextPage);
-            updatePageQueryParameter(nextPage);
-
-            if (nextPage >= pageCount) {
-                setHasMore(false); // Stop infinite scrolling when reaching the last page
-            }
-        } catch (error) {
-            console.error("Error loading more products:", error);
-        } finally {
-            setLoadingMore(false);
-        }
-    };
+    useEffect(() => {
+        setFilters(initialFilters);
+    }, [filters]);
 
     // Update the page query parameter in the URL without reloading the page
     const updatePageQueryParameter = (newPage: number) => {
@@ -67,10 +59,7 @@ export const ProductsGridMobile = ({
                 : [router.query.params]
             : segments;
 
-        const cleanedBasePath = `${path.includes('suche') ? "/suche" : ""}/${pathSegments.join("/")}`;
-
-
-
+        const cleanedBasePath = `${path.includes('suche') ? "/suche" : ""}/${pathSegments.join("/")}`.replace(/\/suche\/suche/, "/suche");
 
         const updatedQuery = { ...router.query, page: newPage };
         delete updatedQuery.params;
@@ -81,8 +70,70 @@ export const ProductsGridMobile = ({
                 : ""
         }`;
 
-        router.push(newUrl, undefined, { shallow: true });
+        router.push(newUrl, undefined, { shallow: true }); // Use shallow routing to update the URL without refreshing
     };
+
+    const loadMoreProductsMobile = async () => {
+        if (loadingMore || page >= pageCount) return;
+
+        setLoadingMore(true);
+
+        try {
+            const nextPage = page + 1;
+            const updatedFilters = { ...filters, page: nextPage };
+            const { data } = await fetchProducts(updatedFilters);
+            setProducts((prevProducts) => [...prevProducts, ...data]);
+            setPage(nextPage);
+            updatePageQueryParameter(nextPage);
+
+            if (nextPage >= pageCount) {
+                setHasMore(false); // Stop infinite scroll when all pages are loaded
+            }
+
+            if (autoLoadCount >= maxCount) {
+                setAutoLoadCount(0);
+            } else {
+                setAutoLoadCount((count) => count + 1);
+            }
+        } catch (error) {
+            console.error("Error loading more products:", error);
+        } finally {
+            setLoadingMore(false);
+        }
+    };
+
+
+    useEffect(() => {
+        const queryPage = parseInt(router.query.page as string) || 1;
+
+        if (queryPage > 1) {
+
+            setLoadingMore(true);
+
+            const loadInitialProducts = async () => {
+                setIsLoading(true);
+                try {
+                    const totalProducts = queryPage * filters.pageSize;
+                    const updatedFilters = {
+                        ...filters,
+                        page: 1,
+                        pageSize: totalProducts,
+                    };
+                    const { data } = await fetchProducts(updatedFilters);
+                    setProducts(data);
+                    setPage(queryPage);
+                    setAutoLoadCount(queryPage - 1);
+                } catch (error) {
+                    console.error("Error loading initial products:", error);
+                } finally {
+                    setLoadingMore(false);
+                    setIsLoading(false);
+                }
+            };
+
+            loadInitialProducts();
+        }
+    }, []);
 
     return (
         <>
@@ -91,10 +142,10 @@ export const ProductsGridMobile = ({
             ) : (
                 <div className="w-full flex flex-col items-center">
                     <div className="w-full mx-auto p-0">
-                        {/* Display products using InfiniteScroll */}
+                        {/* Infinite Scroll Component */}
                         <InfiniteScroll
                             dataLength={products.length}
-                            next={loadMoreProducts}
+                            next={loadMoreProductsMobile}
                             hasMore={hasMore}
                             loader={
                                 <div className="text-center">
@@ -110,11 +161,33 @@ export const ProductsGridMobile = ({
                         >
                             <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-6 gap-1 sm:gap-4 py-4">
                                 {products.map((product, index) => (
-                                    <ProductCard key={index} product={product}/>
+                                    <ProductCard key={index} product={product} />
                                 ))}
                             </div>
                         </InfiniteScroll>
                     </div>
+
+                    {/* Mobile "Load More" Button */}
+                    {autoLoadCount >= maxCount && hasMore && (
+                        <div className="block sm:hidden mt-4">
+                            <Button
+                                className={'text-white bg-blue-500'}
+                                onClick={loadMoreProductsMobile}
+                                disabled={page >= pageCount || loadingMore}
+                            >
+                                {loadingMore ? (
+                                    <>
+                                        <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                                        Laden...
+                                    </>
+                                ) : page < pageCount ? (
+                                    "Mehr laden"
+                                ) : (
+                                    "Keine weiteren Produkte"
+                                )}
+                            </Button>
+                        </div>
+                    )}
                 </div>
             )}
         </>
