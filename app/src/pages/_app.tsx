7b1e034b47d806708bdf9536c26506d5d56268cd
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2024-10-14 20:05:38 +0200

    wip

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -16,6 +16,7 @@ import {TooltipProvider} from "@/shadcn/components/ui/tooltip";
 import {ProductSheetProvider} from "@/lib/context/product-sheet-context";
 import {Suspense} from "react";
 import ErrorBoundary from "@/components/error-boundary";
+import NextProgress from "next-progress";
 
 const ToastContainer = dynamic(
     () => import('react-toastify').then((module) => module.ToastContainer),
@@ -52,6 +53,7 @@ function CustomApp({
                                 <ModalProvider>
                                     <CartProvider>
                                         <DefaultSeo/>
+                                        <NextProgress height="2px" color="#009f7f" options={{ showSpinner: false }}/>
                                         {getLayout(<Component {...pageProps} />)}
                                         <ToastContainer autoClose={2000} theme="colored"/>
                                     </CartProvider>
